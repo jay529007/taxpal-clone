8984c93fd5c9a095144bede33b500d31e58c8172
jay529007 <jaysatapara.3@gmail.com>
2025-03-19 18:58:18 +0530

    section-2
    fixed image problem

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 //     alert("Div clicked!");
 //   }
 window.onload = () => {
-  displayImage("payroll.png"); // default image
+  displayImage("payroll.png"); //  default image 
 };
 
 function displayImage(imageName) {
   const imageContainer = document.getElementById("image-container");
   imageContainer.style.backgroundImage = `url('/Assets/images/${imageName}')`;
-  imageContainer.style.backgroundSize = "cover"; 
-  // imageContainer.style.backgroundSize = "contain"; 
-  // imageContainer.style.backgroundPosition = "Center"; 
+  // imageContainer.style.backgroundSize = "cover"; 
+  imageContainer.style.backgroundSize = "contain"; 
+  imageContainer.style.backgroundPosition = "Center"; 
   imageContainer.style.backgroundRepeat = "no-repeat"; 
 }
