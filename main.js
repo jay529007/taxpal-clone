dbc5dfb9f6c36fd8155191cd6160d2a2a8d9a025
jay529007 <jaysatapara.3@gmail.com>
2025-03-19 18:23:12 +0530

    New Features
    changes in page 6

---
+++ main.js
@@ -0,0 +1,15 @@
+// function handleClick() {
+//     alert("Div clicked!");
+//   }
+window.onload = () => {
+  displayImage("payroll.png"); // default image
+};
+
+function displayImage(imageName) {
+  const imageContainer = document.getElementById("image-container");
+  imageContainer.style.backgroundImage = `url('/Assets/images/${imageName}')`;
+  imageContainer.style.backgroundSize = "cover"; 
+  // imageContainer.style.backgroundSize = "contain"; 
+  // imageContainer.style.backgroundPosition = "Center"; 
+  imageContainer.style.backgroundRepeat = "no-repeat"; 
+}
