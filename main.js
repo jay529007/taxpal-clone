c654c98545b4484681b9adf11b8dedd47d7e28df
jay529007 <jaysatapara.3@gmail.com>
2025-03-21 12:19:34 +0530

    page-2
    recreated page-2
    because it wasn't responsive

--- main.js
+++ main.js
@@ -8,8 +8,17 @@ window.onload = () => {
 function displayImage(imageName) {
   const imageContainer = document.getElementById("image-container");
   imageContainer.style.backgroundImage = `url('/Images/${imageName}')`;
-  // imageContainer.style.backgroundSize = "cover"; 
-  imageContainer.style.backgroundSize = "contain"; 
-  imageContainer.style.backgroundPosition = "Center"; 
-  imageContainer.style.backgroundRepeat = "no-repeat"; 
+  imageContainer.style.backgroundSize = "cover"; 
+  // imageContainer.style.backgroundSize = "contain"; 
+  // imageContainer.style.backgroundPosition = "Center"; 
+  // imageContainer.style.backgroundRepeat = "no-repeat"; 
+}
+
+function showSidebar() {
+  const sidebar = document.querySelector(".menu-p");
+  sidebar.style.display = "block";
+}
+function hideSidebar() {
+  const sidebar = document.querySelector(".menu-p");
+  sidebar.style.display = "none";
 }
